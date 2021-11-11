e6ff0affd91e55d446bc95c3f4ca4dc51241a97e
Alastair Green <agreen@friendmts.com>
2021-11-11 13:30:18 +0000

    Fix errors
    * Update to use syntax compatible with terraform 1.0.1
    * Add missing security group for replication group

--- ssh.tf
+++ ssh.tf
@@ -16,24 +16,25 @@ data "aws_ami" "ubuntu-1604" {
 }
 
 data "template_file" "startup" {
-  template = "${file("${path.module}/templates/startup.sh.tpl")}"
+  template = file("${path.module}/templates/startup.sh.tpl")
 }
 
 resource "aws_key_pair" "elasticcache" {
   key_name   = "${var.namespace}-elasticcache"
-  public_key = "${file("${var.public_key_path}")}"
+  public_key = file("${var.public_key_path}")
 }
 
 resource "aws_instance" "ssh_host" {
-  ami           = "${data.aws_ami.ubuntu-1604.id}"
+  ami           = data.aws_ami.ubuntu-1604.id
   instance_type = "t2.nano"
-  key_name      = "${aws_key_pair.elasticcache.id}"
+  key_name      = aws_key_pair.elasticcache.id
 
-  subnet_id              = "${element(aws_subnet.default.*.id,0)}"
+  subnet_id              = element(aws_subnet.default.*.id, 0)
   vpc_security_group_ids = ["${aws_security_group.default.id}"]
-  user_data              = "${data.template_file.startup.rendered}"
+  user_data              = data.template_file.startup.rendered
 
-  tags = "${map(
-    "Name", "${var.namespace}-ssh-host",
-  )}"
+  tags = {
+    Name = "${var.namespace}-ssh-host"
+  }
+
 }
